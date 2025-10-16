f8bc826216da1ec505bed755ea77704152485eae
Nixluna <contact@nixluna.com>
2025-10-16 17:46:14 -0400

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: ["./pages/**/*.{js,ts,jsx,tsx}","./components/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        midnight: "#0B0C24",
+        snow: "#FFFFFF",
+        gold: "#D4AF37",
+        lunar: "#C0C0C0"
+      },
+      fontFamily: {
+        title: ["Cinzel", "serif"],
+        body: ["Montserrat", "sans-serif"]
+      }
+    }
+  },
+  plugins: [],
+}
